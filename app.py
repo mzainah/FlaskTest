9ef5f4d53b7c3ddd92a7ba3da69a0db5ae123639
mzainah <mzainah@yahoo.com>
2025-08-06 14:30:04 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # app.py
+import os
 from flask import Flask, render_template, request, redirect, url_for
 from database import get_all_contacts, add_contact, delete_contact
 
@@ -25,4 +26,4 @@ def about():
     return render_template("about.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
